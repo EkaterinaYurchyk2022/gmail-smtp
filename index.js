512ba1756f3b0d169bf01e59f5d83ec6a2160f90
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-11-07 19:27:39 +0300

    initial commot

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 25,
+    secure: false,
+    tls: {
+        rejectUnauthorized: false
+    },
+    auth: {
+        user: 'katyayurchyk@gmail.com',
+        pass: 'qpbt apxe mjlj nuwe'
+    }
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.post('/sendMessage', async function (req, res) {
+    let {name, email, message} = req.body
+
+    let info = await transporter.sendMail({
+        from: 'Message from my portfolio contact form',
+        to: 'ekaterina.yurchyk.s@gmail.com',
+        subject: 'Message from my portfolio contact form',
+        //text: '',
+        html: `<b>Привет. Тебе письмо</b>
+<div>
+${name}
+</div>\`
+<div>
+${email}
+</div>\`
+<div>
+${message}
+</div>`
+    })
+    res.send('ok!')
+})
+
+app.listen(3010, function () {
+    console.log('Example app listening on port 3000!')
+})
+
+
+
